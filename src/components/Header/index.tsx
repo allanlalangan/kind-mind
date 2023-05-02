614b940ec1c4bf7ba3807feb2139709b1e3a1ec6
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2023-05-02 12:43:54 -0700

    fix header responsive position

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {
 
 export default function Header({ navIsOpen, setNavIsOpen }: HeaderProps) {
   return (
-    <header className="col-span-12 row-span-1 flex items-center justify-center rounded-lg bg-neutral-100 p-2 shadow-sm md:justify-start">
+    <header className="fixed left-2 right-2 top-2 col-span-12 row-span-1 flex items-center justify-center rounded-lg bg-neutral-100 p-2 shadow-sm md:static md:justify-start">
       <Link
         href="/"
         className="btn-ghost btn mx-2 text-2xl normal-case text-primary"
