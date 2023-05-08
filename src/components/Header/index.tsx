367f3e273c02813b44faaca9da3f49084b2c2cde
allanlalangan <allanlalangan@gmail.com>
2023-05-08 10:21:59 -0700

    add logout button to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 type HeaderProps = {
@@ -6,6 +9,7 @@ type HeaderProps = {
 };
 
 export default function Header({ navIsOpen, setNavIsOpen }: HeaderProps) {
+  const session = useSession();
   return (
     <header className="fixed left-2 right-2 top-2 col-span-12 row-span-1 flex items-center justify-center rounded-lg bg-neutral-100 p-2 shadow-sm md:static md:justify-between">
       <Link
@@ -14,9 +18,18 @@ export default function Header({ navIsOpen, setNavIsOpen }: HeaderProps) {
       >
         kindMind
       </Link>
-      <Link href="/login" className="btn-ghost btn-sm btn mx-2">
-        Login
-      </Link>
+      {!!session.data ? (
+        <button
+          onClick={() => void signOut()}
+          className="btn-ghost btn-sm btn mx-2"
+        >
+          Logout
+        </button>
+      ) : (
+        <Link href="/login" className="btn-ghost btn-sm btn mx-2">
+          Login
+        </Link>
+      )}
     </header>
   );
 }
