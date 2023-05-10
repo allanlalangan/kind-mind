98e8c7d95d51e5f8d9fca9459568c6c7ba1724c2
allanlalangan <allanlalangan@gmail.com>
2023-05-10 11:35:51 -0700

    fix: update z-index on navigation components

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@ import LoginForm from "~/components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-8">
+    <main className="flex items-center justify-center p-8">
       <LoginForm />
     </main>
   );
