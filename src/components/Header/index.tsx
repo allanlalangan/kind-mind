aca541b620d5d75657062f06eeccc06dde59b4b7
allanlalangan <allanlalangan@gmail.com>
2023-05-10 11:19:24 -0700

    display user avatar in header when logged in update redirect on signOut

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 
-type HeaderProps = {
-  navIsOpen: boolean;
-  setNavIsOpen: (isOpen: boolean) => void;
-};
-
-export default function Header({ navIsOpen, setNavIsOpen }: HeaderProps) {
+export default function Header() {
   const session = useSession();
   return (
-    <header className="fixed left-2 right-2 top-2 col-span-12 row-span-1 flex items-center justify-center rounded-lg bg-neutral-100 p-2 shadow-sm md:static md:justify-between">
+    <header className="fixed left-2 right-2 top-2 col-span-12 row-span-1 flex items-center justify-between rounded-lg bg-neutral-100 p-2 shadow-sm md:static md:justify-between">
       <Link
         href="/"
         className="btn-ghost btn mx-2 text-2xl normal-case text-primary"
@@ -19,12 +15,23 @@ export default function Header({ navIsOpen, setNavIsOpen }: HeaderProps) {
         kindMind
       </Link>
       {!!session.data ? (
-        <button
-          onClick={() => void signOut()}
-          className="btn-ghost btn-sm btn mx-2"
-        >
-          Logout
-        </button>
+        <section className="flex items-center">
+          <button
+            onClick={() => void signOut({ callbackUrl: "/login" })}
+            className="btn-ghost btn-sm btn mx-2"
+          >
+            Logout
+          </button>
+          <button className="btn-circle btn">
+            <Image
+              className="rounded-full"
+              alt={`${session?.data?.user?.name || "user"}'s avatar`}
+              src={session?.data?.user?.image || ""}
+              width={200}
+              height={200}
+            />
+          </button>
+        </section>
       ) : (
         <Link href="/login" className="btn-ghost btn-sm btn mx-2">
           Login
