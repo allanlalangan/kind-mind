b7512f3c92833f8f91f018bebc556dba852e7249
allanlalangan <allanlalangan@gmail.com>
2023-05-10 21:42:28 -0700

    update tailwind config color palette update section and nav component styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,14 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: ({ colors }) => ({
+        primary: colors.indigo,
+        secondary: colors.orange,
+        accent: colors.sky,
+        base: colors.gray,
+        neutral: colors.slate,
+      }),
+    },
   },
 } satisfies Config;
