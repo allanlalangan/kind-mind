02d11b9db2b5b4ad2ca7dab0826032081271f18f
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2023-05-14 10:09:38 -0700

    refactor to github auth

---
+++ src/components/GitHubSignInButton/index.tsx
@@ -0,0 +1,83 @@
+import { signIn, useSession } from "next-auth/react";
+import { useState } from "react";
+
+export default function GitHubSignInButton() {
+  const session = useSession();
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <button
+      disabled={
+        isLoading || !!session?.data?.user || session?.status === "loading"
+      }
+      onClick={() => {
+        void signIn("github", {
+          callbackUrl: "http://localhost:3000/dashboard",
+        });
+        setIsLoading(true);
+      }}
+      className="flex w-64 items-center justify-center rounded bg-slate-600 px-6 pb-2 pt-2.5 text-white transition hover:bg-slate-700 active:bg-slate-800 disabled:bg-slate-700/40"
+    >
+      {session.status === "loading" || isLoading ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+        >
+          <g stroke="currentColor">
+            <circle
+              cx="12"
+              cy="12"
+              r="9.5"
+              fill="none"
+              stroke-linecap="round"
+              stroke-width="3"
+            >
+              <animate
+                attributeName="stroke-dasharray"
+                calcMode="spline"
+                dur="1.5s"
+                keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+                keyTimes="0;0.475;0.95;1"
+                repeatCount="indefinite"
+                values="0 150;42 150;42 150;42 150"
+              />
+              <animate
+                attributeName="stroke-dashoffset"
+                calcMode="spline"
+                dur="1.5s"
+                keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+                keyTimes="0;0.475;0.95;1"
+                repeatCount="indefinite"
+                values="0;-16;-59;-59"
+              />
+            </circle>
+            <animateTransform
+              attributeName="transform"
+              dur="2s"
+              repeatCount="indefinite"
+              type="rotate"
+              values="0 12 12;360 12 12"
+            />
+          </g>
+        </svg>
+      ) : (
+        <>
+          <span>Login With GitHub</span>
+          <svg
+            className="ml-1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.09.09 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.09 16.09 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02zM8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12zm6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12z"
+            />
+          </svg>
+        </>
+      )}
+    </button>
+  );
+}
