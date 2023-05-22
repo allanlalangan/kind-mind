5475f5e6fc81f383803627577f535144e93e8870
allanlalangan <allanlalangan@gmail.com>
2023-05-22 11:29:34 -0700

    separate LoadingSpinner component, update Github button

--- src/components/GitHubSignInButton/index.tsx
+++ src/components/GitHubSignInButton/index.tsx
@@ -1,5 +1,6 @@
 import { signIn, useSession } from "next-auth/react";
 import { useState } from "react";
+import LoadingSpinner from "../LoadingSpinner";
 
 export default function GitHubSignInButton() {
   const session = useSession();
@@ -18,49 +19,7 @@ export default function GitHubSignInButton() {
       className="flex w-64 items-center justify-center rounded bg-slate-600 px-6 pb-2 pt-2.5 text-white transition hover:bg-slate-700 active:bg-slate-800 disabled:bg-slate-700/40"
     >
       {session.status === "loading" || isLoading ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 24 24"
-        >
-          <g stroke="currentColor">
-            <circle
-              cx="12"
-              cy="12"
-              r="9.5"
-              fill="none"
-              stroke-linecap="round"
-              stroke-width="3"
-            >
-              <animate
-                attributeName="stroke-dasharray"
-                calcMode="spline"
-                dur="1.5s"
-                keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-                keyTimes="0;0.475;0.95;1"
-                repeatCount="indefinite"
-                values="0 150;42 150;42 150;42 150"
-              />
-              <animate
-                attributeName="stroke-dashoffset"
-                calcMode="spline"
-                dur="1.5s"
-                keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-                keyTimes="0;0.475;0.95;1"
-                repeatCount="indefinite"
-                values="0;-16;-59;-59"
-              />
-            </circle>
-            <animateTransform
-              attributeName="transform"
-              dur="2s"
-              repeatCount="indefinite"
-              type="rotate"
-              values="0 12 12;360 12 12"
-            />
-          </g>
-        </svg>
+        <LoadingSpinner />
       ) : (
         <>
           <span>Login With GitHub</span>
