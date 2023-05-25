95e5ac5c674c8fd41bb5ae9da54a0e0578508080
allanlalangan <allanlalangan@gmail.com>
2023-05-25 01:09:21 -0700

    update content states on discard, cancel, and save modal button events refetch on save, move DialogModals and useEditor into calling components

--- src/components/TipTapEditor/index.tsx
+++ src/components/TipTapEditor/index.tsx
@@ -2,46 +2,29 @@ import { api } from "~/utils/api";
 import TipTapMenuBar from "../TipTapMenuBar";
 
 import styles from "./styles.module.css";
-import { EditorContent, useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
+import { type Editor, EditorContent } from "@tiptap/react";
 import { useEffect } from "react";
 
 type TipTapEditorProps = {
+  editor: Editor | null;
+  refetchEntry?: () => void;
+  modalIsOpen: boolean;
+  setModalIsOpen: (value: boolean) => void;
   content?: string;
+  tempContent?: string;
+  setTempContent?: (value: string) => void;
   isEditable?: boolean;
+  setIsEditable?: (value: boolean) => void;
   titleInputValue?: string;
   setTitleInputValue: (value: string) => void;
 };
 
 export default function TipTapEditor({
-  content,
+  editor,
   isEditable,
   titleInputValue,
   setTitleInputValue,
 }: TipTapEditorProps) {
-  const editor = useEditor({
-    extensions: [
-      StarterKit.configure({
-        bulletList: {
-          keepMarks: true,
-          keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
-        },
-        orderedList: {
-          keepMarks: true,
-          keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
-        },
-      }),
-    ],
-    editorProps: {
-      attributes: {
-        class:
-          "prose bg-base-100/40 dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl p-4 pt-6 focus:outline-none",
-      },
-    },
-    content: content || "",
-    editable: isEditable || true,
-  });
-
   useEffect(() => {
     editor?.setEditable(Boolean(isEditable));
   }, [editor, isEditable]);
@@ -54,38 +37,33 @@ export default function TipTapEditor({
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    createNewEntry.mutate({
-      title: titleInputValue || "Untitled",
-      content: editor?.options.content as string,
-    });
+    // createNewEntry.mutate({
+    //   title: titleInputValue || "Untitled",
+    //   content: editor?.getHTML() as string,
+    // });
   };
 
   return (
-    <form
-      onSubmit={(e) => e.preventDefault()}
-      className="relative col-span-12 -mx-4 flex flex-col rounded"
-    >
+    <>
       {isEditable && (
-        <>
-          <section className="z-10 bg-base-900 pb-2 shadow-md md:pt-2">
-            <div className="">
-              <input
-                onChange={(e) => setTitleInputValue(e.target.value)}
-                value={titleInputValue}
-                className="w-full rounded-t bg-transparent p-4 text-4xl font-semibold text-primary-500 placeholder:font-semibold placeholder:text-primary-500/75"
-                placeholder="Entry Title..."
-                type="text"
-                name="title"
-                id="title"
-              />
+        <section className="z-10 -mx-4 bg-base-900 pb-2 shadow-md md:pt-2">
+          <div className="">
+            <input
+              onChange={(e) => setTitleInputValue(e.target.value)}
+              value={titleInputValue}
+              className="w-full rounded-t bg-transparent p-4 text-4xl font-semibold text-primary-500 placeholder:font-semibold placeholder:text-primary-500/75"
+              placeholder="Entry Title..."
+              type="text"
+              name="title"
+              id="title"
+            />
 
-              <TipTapMenuBar editor={editor} />
-            </div>
-          </section>
-        </>
+            <TipTapMenuBar editor={editor} />
+          </div>
+        </section>
       )}
 
       <EditorContent className={styles.ProseMirror} editor={editor} />
-    </form>
+    </>
   );
 }
--- src/pages/journal/create/index.tsx
+++ src/pages/journal/create/index.tsx
@@ -1,3 +1,5 @@
+import { useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 import Link from "next/link";
 import { useState } from "react";
 import DashboardLayout from "~/components/DashboardLayout";
@@ -5,7 +7,37 @@ import TipTapEditor from "~/components/TipTapEditor";
 import { type NextPageWithLayout } from "~/pages/_app";
 
 const CreateJournalEntryPage: NextPageWithLayout = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   const [titleInputValue, setTitleInputValue] = useState("");
+  const [tempContent, setTempContent] = useState("");
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        bulletList: {
+          keepMarks: true,
+          keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        },
+        orderedList: {
+          keepMarks: true,
+          keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        },
+      }),
+    ],
+    editorProps: {
+      attributes: {
+        class:
+          "-mx-4 prose bg-base-100/40 dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl p-4 pt-6 focus:outline-none",
+      },
+    },
+    content: tempContent,
+    editable: true,
+    onUpdate: ({ editor }) => {
+      setTempContent?.(editor.getHTML());
+    },
+  });
+
   return (
     <>
       <section className="mb-2">
@@ -18,25 +50,36 @@ const CreateJournalEntryPage: NextPageWithLayout = () => {
         &gt;
         <span className="px-1">Create New Entry</span>
       </section>
-      <div className="-mx-4 grid grid-cols-2 gap-1 bg-base-900 p-2">
-        <button
-          type="button"
-          className="col-span-1 rounded border border-accent-200 bg-base-400/20 p-2 text-accent-200 transition hover:bg-base-400/30 active:bg-base-400/40"
-        >
-          Save Draft
-        </button>
-        <button
-          type="submit"
-          className="col-span-1 rounded bg-accent-200 p-2 text-primary-800 transition hover:bg-accent-300 active:bg-accent-400"
-        >
-          Submit Entry
-        </button>
-      </div>
-      <TipTapEditor
-        titleInputValue={titleInputValue}
-        setTitleInputValue={setTitleInputValue}
-        isEditable={true}
-      />
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="relative col-span-12 -mx-0 flex flex-col rounded"
+      >
+        <div className="-mx-4 grid grid-cols-2 gap-1 bg-base-900 p-2">
+          <button
+            type="button"
+            className="col-span-1 rounded border border-accent-200 bg-base-400/20 p-2 text-accent-200 transition hover:bg-base-400/30 active:bg-base-400/40"
+          >
+            Save Draft
+          </button>
+          <button
+            onClick={() => console.log(tempContent)}
+            type="button"
+            className="col-span-1 rounded bg-accent-200 p-2 text-primary-800 transition hover:bg-accent-300 active:bg-accent-400"
+          >
+            Submit Entry
+          </button>
+        </div>
+        <TipTapEditor
+          editor={editor}
+          modalIsOpen={modalIsOpen}
+          setModalIsOpen={setModalIsOpen}
+          content={tempContent}
+          setTempContent={setTempContent}
+          titleInputValue={titleInputValue}
+          setTitleInputValue={setTitleInputValue}
+          isEditable={true}
+        />
+      </form>
     </>
   );
 };
