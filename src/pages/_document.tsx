7ca4a911859cf591e6e66072a25a5abbd8c4597e
allanlalangan <allanlalangan@gmail.com>
2023-06-06 14:18:35 -0700

    create notch tailwind classes, update viewport meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="relative flex min-h-screen items-start justify-start p-2">
+      <body className="relative flex min-h-screen items-start justify-start p-2 safe-top safe-left safe-right safe-bottom">
         <Main />
         <NextScript />
       </body>
