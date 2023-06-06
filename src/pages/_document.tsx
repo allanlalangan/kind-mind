b504a92bba143f8ef3d59bf3798abad69ca28408
allanlalangan <allanlalangan@gmail.com>
2023-06-06 14:02:42 -0700

    apply gradient bg to html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="relative flex min-h-screen items-start justify-start bg-gradient-to-br from-primary-400 to-secondary-400 p-2">
+      <body className="relative flex min-h-screen items-start justify-start p-2">
         <Main />
         <NextScript />
       </body>
